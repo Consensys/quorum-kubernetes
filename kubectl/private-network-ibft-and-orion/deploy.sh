347e331c48a6e37de9af38fd4daed0a538fec9cb
marlowl <marlowlieshout@hotmail.com>
2020-01-18 13:12:16 +0100

    cleaned up deploy script

--- kubectl/private-network-ibft-and-orion/deploy.sh
+++ kubectl/private-network-ibft-and-orion/deploy.sh
@@ -7,22 +7,6 @@ kubectl apply -f configmap/orion-configmap.yaml
 kubectl apply -f configmap/prometheus-configmap.yaml
 kubectl apply -f configmap/grafana-configmap.yaml
 
-kubectl apply -f services/validator1-service.yaml
-kubectl apply -f services/validator2-service.yaml
-kubectl apply -f services/validator3-service.yaml
-kubectl apply -f services/validator4-service.yaml
-kubectl apply -f services/node-service.yaml
-kubectl apply -f services/node1privacy-service.yaml
-kubectl apply -f services/node2privacy-service.yaml
-kubectl apply -f services/node3privacy-service.yaml
-kubectl apply -f services/node4privacy-service.yaml
-kubectl apply -f services/orion1-service.yaml
-kubectl apply -f services/orion2-service.yaml
-kubectl apply -f services/orion3-service.yaml
-kubectl apply -f services/orion4-service.yaml
-kubectl apply -f services/prometheus-service.yaml
-kubectl apply -f services/grafana-service.yaml
-
 kubectl apply -f deployments/orion1-deployment.yaml
 kubectl apply -f deployments/orion2-deployment.yaml
 kubectl apply -f deployments/orion3-deployment.yaml
@@ -32,10 +16,19 @@ kubectl apply -f deployments/validator2-deployment.yaml
 kubectl apply -f deployments/validator3-deployment.yaml
 kubectl apply -f deployments/validator4-deployment.yaml
 kubectl apply -f deployments/node-deployment.yaml
-kubectl apply -f deployments/node1privacy-deployment.yaml
-kubectl apply -f deployments/node2privacy-deployment.yaml
-kubectl apply -f deployments/node3privacy-deployment.yaml
-kubectl apply -f deployments/node4privacy-deployment.yaml
-
 kubectl apply -f deployments/prometheus-deployment.yaml
 kubectl apply -f deployments/grafana-deployment.yaml
+
+kubectl apply -f services/validator1-service.yaml
+kubectl apply -f services/validator2-service.yaml
+kubectl apply -f services/validator3-service.yaml
+kubectl apply -f services/validator4-service.yaml
+kubectl apply -f services/node-service.yaml
+kubectl apply -f services/orion1-service.yaml
+kubectl apply -f services/orion2-service.yaml
+kubectl apply -f services/orion3-service.yaml
+kubectl apply -f services/orion4-service.yaml
+kubectl apply -f services/prometheus-service.yaml
+kubectl apply -f services/grafana-service.yaml
+
+
