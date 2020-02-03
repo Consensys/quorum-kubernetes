a103cbc819ffffb50f91e57f3eecaa9197314630
marlowl <marlowlieshout@hotmail.com>
2020-02-03 09:19:19 +0100

    deployments as last deploy step

--- kubectl/private-network-ibft/deploy.sh
+++ kubectl/private-network-ibft/deploy.sh
@@ -1,6 +1,6 @@
 kubectl apply -f namespace/
-kubectl apply -f deployments/
 kubectl apply -f rbac/
 kubectl apply -f secrets/
 kubectl apply -f configmap/
 kubectl apply -f services/
+kubectl apply -f deployments/
