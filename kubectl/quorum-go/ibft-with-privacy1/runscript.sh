92e5ec821d6f4488c0c5eb5928ae0a38fe3755b4
Joshua Fernandes <joshua.fernandes@consensys.net>
2020-10-20 13:47:43 +1000

    v1-works-sans-dns

--- kubectl/quorum-go/ibft-with-privacy1/runscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -z $1 ] || [ ! -f $1 ]; then
-  echo "Please provide a valid script file to execute as the first parameter (i.e. private_contract.js)" >&2
-  exit 1
-fi
-
-NODE_NUMBER=1
-POD=$(kubectl get pod --field-selector=status.phase=Running -o name | grep quorum-node$NODE_NUMBER)
-kubectl $NAMESPACE exec -it $POD -c quorum -- /etc/quorum/qdata/contracts/runscript.sh /etc/quorum/qdata/contracts/$1
--- kubectl/quorum-go/ibft-with-privacy1/stop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kubectl delete -f out -f out/deployments
